02160abbafd31762715558442b4f5cd5d9fe7f38
Rohit-209425 <Rohit-209425@YAANTRA-DEVELOP>
2024-02-26 18:57:17 +0530

    Add project files.

---
+++ WebApplication1/Controllers/AuthorApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class AuthorApiController : ApiController
+    {
+        StudentdetailsEntities db = new StudentdetailsEntities();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetAuthors()
+        {
+            List<Author> list = db.Authors.ToList();
+            return Ok(list);
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetAuthorsById(int id)
+        {
+            var auth = db.Authors.Where(model => model.id == id).FirstOrDefault();
+            return Ok(auth);
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult AuthInsert(Author a)
+        {
+            db.Authors.Add(a);
+            db.SaveChanges();
+            return Ok("Author inserted successfully.");
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/BookApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize]
+    public class BookApiController : ApiController
+    {
+        StudentdetailsEntities db = new StudentdetailsEntities();
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetBooks()
+        {
+            List<Book> list = db.Books.ToList();
+            return Ok(list);
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetBooksByAuthorId(int id)
+        {
+            var book = db.Books.Where(model => model.author_id == id);
+            return Ok(book);
+        }
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult BookInsert(Book b)
+        {
+            var existingBook = db.Books.FirstOrDefault(x => x.book_name == b.book_name && x.author_id == b.author_id);
+
+            if (existingBook != null)
+            {
+                return BadRequest("Book with same name and author already exists.");
+            }
+            else
+            {
+                db.Books.Add(b);
+                db.SaveChanges();
+                return Ok("Book inserted successfully.");
+            }
+        }
+    }
+}
